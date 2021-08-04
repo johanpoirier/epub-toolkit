b212e5d7da6d50b8ac28bfe7b9623f5d9b74af28
Johan Poirier <johan.poirier@gmail.com>
2021-08-04 09:58:06 +0200

    Add EpubCFI to exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import polyfills from './utils/polyfills';
 import Explorer from './Explorer';
 import Lcp from './Lcp';
+import EpubCFI from './cfi/epubcfi';
 
 polyfills();
 
-export {Explorer, Lcp};
+export {Explorer, Lcp, EpubCFI};
